e64a4c2709b513cd10ba8484dce6192ae7bd8372
dee <178827694+dee797@users.noreply.github.com>
2024-09-04 17:20:28 -0600

    Finished all exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    
+    if ((!Number.isInteger(start) || !Number.isInteger(end)) || (start < 0 || end < 0)) {
+        return "ERROR";
+    } else if (end > start) {
+        for (start; start <= end; start++) {
+            sum += start;
+        }
+    } else {
+        for (start; start >= end; start--) {
+            sum += start;
+        }
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
