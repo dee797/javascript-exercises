5617056e1d49f8f3c0bd9dc098b7d4797b306d4a
dee <178827694+dee797@users.noreply.github.com>
2024-09-07 17:06:12 -0600

    Finished calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(...args) {
+  //apparently you shouldn't use += to accumulate total (same with -=)
+  return args.reduce((total, currentValue) => total + currentValue);
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	return args.reduce((total, currentValue) => total - currentValue);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  // Second argument (0) sets an initial value to total, useful if arr is an empty array
+	const sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(...args) {
+	return args.reduce((accumulator, currentValue) => accumulator ** currentValue)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  
+  //at this point arr is a sparse array since values between arr[0]
+  //and arr[num-1] are undefined, if num is greater than 2
+  //the map method cannot iterate over all elements in a sparse array
+  let arr = [1];
+  arr[num-1] = num;
+
+  //using the spread syntax (...arr) will make the map method work on
+  //sparse arrays
+  [...arr].map((value, index) => {
+    value = index + 1;
+    arr[index] = value;
+  });
+
+
+	return arr.reduce((accumulator, currentValue) => accumulator * currentValue);
 };
 
 // Do not edit below this line
