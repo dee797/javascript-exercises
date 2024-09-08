8c74190f9161e1d4703e9c940375d516d61cb2d3
dee <178827694+dee797@users.noreply.github.com>
2024-09-07 18:12:39 -0600

    Finised palindrome exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //split 'string' into substrings wherever there are non-alphanumeric characters (negative match
+    //is indicated by the carot ^ in the regex pattern), then join them back together.
+    const regex = new RegExp("[^A-Za-z0-9]");
+    const newString = string.split(regex).join("").toLowerCase();
 
+    const stringBackwards = newString.split("").reverse().join("");
+
+    if (stringBackwards === newString) return true;
+    else return false;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
