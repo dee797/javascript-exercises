22dd4d801dcf05c13e0c150c5a43429a3e9deb86
dee <178827694+dee797@users.noreply.github.com>
2024-09-09 14:07:26 -0600

    Finish findTheOldest exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const today = new Date();
 
+    const newPeople = people.map((person) => {
+        if ("yearOfDeath" in person) {
+            person["age"] = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person["age"] = today.getFullYear() - person.yearOfBirth;
+        }
+        return person;
+    });
+
+    newPeople.sort((secondPerson, firstPerson) => secondPerson.age - firstPerson.age);
+
+    return newPeople[newPeople.length - 1];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
