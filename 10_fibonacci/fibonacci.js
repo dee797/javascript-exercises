40893efbe7464f9e490cc6d261c263014346849d
dee <178827694+dee797@users.noreply.github.com>
2024-09-09 11:38:04 -0600

    Finish fibonacci exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let arr = [0, 1];
 
+    input = parseInt(input);
+    if (input < 0 || !Number.isInteger(input)) return "OOPS";
+
+    arr[input] = input;
+    [...arr].map((currentValue, index) => {
+        if (index >= 2) {
+            currentValue = arr[index - 1] + arr[index - 2];
+            arr[index] = currentValue;
+        }
+    });
+
+    return arr[input];
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
